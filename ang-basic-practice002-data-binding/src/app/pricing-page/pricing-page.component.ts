35fd1895953ac34b3bee169930cbe78192932496
cherryApp <j.cserko@gmail.com>
2021-01-26 10:36:18 +0100

    Feat: lesson.

---
+++ ang-basic-practice002-data-binding/src/app/pricing-page/pricing-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+interface IPriceItem {
+  name: string;
+  price: number;
+  options: string[];
+  btnText: string;
+}
+
+@Component({
+  selector: 'app-pricing-page',
+  templateUrl: './pricing-page.component.html',
+  styleUrls: ['./pricing-page.component.scss']
+})
+export class PricingPageComponent implements OnInit {
+
+  priceTable: IPriceItem[] = [
+    {name: 'Free', price: 0, options: [], btnText: ''}
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
