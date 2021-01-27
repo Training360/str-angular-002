0e0396dcbb41656ad4b73c088c8f59847754ffed
cherryApp <j.cserko@gmail.com>
2021-01-27 11:28:17 +0100

    Feat: step02.

--- angular-components-pipes/src/app/app.component.ts
+++ angular-components-pipes/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './model/user';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,39 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Data Binding Example';
 
+  subTitle: string = 'Vadas Gabi csinálta.';
+
   btnDisabled: boolean = false;
+
+  users: User[] = [];
+
+  constructor() {
+    setTimeout(() => {
+      this.users = [
+        new User(1, 'Jani', 'j@gmail.com'),
+        new User(2, 'Juli', 'j@gmail.com'),
+        new User(3, 'Józsi', 'j@gmail.com'),
+      ];
+    }, 10000);
+  }
 
   onSwitchDisabled(): void {
     this.btnDisabled = !this.btnDisabled;
+  }
+
+  onShowGreeting(): void {
+    alert('Szia, én vagyok a User bizony.');
+  }
+
+  onDeleteUser(user: User): void {
+    // tslint:disable-next-line: prefer-for-of
+    let index = 0;
+    for (let i = 0; i < this.users.length; i++) {
+      if (user.id === this.users[i].id) {
+        index = i;
+      }
+    }
+
+    this.users.splice(index, 1);
   }
 }
